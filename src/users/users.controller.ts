8c735beb9998560e6041d0ac08f58257cae1b413
Eduardo Schulz <eduardo.schulz68@gmail.com>
2024-03-13 19:33:01 -0300

    feat: create user to access the ocurrences

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,10 +18,10 @@ export class UsersController {
 
   @Post()
   @HttpCode(201)
-  create(@Body() createUserDto: CreateUserDto): Promise<any> {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto): Promise<any> {
+    return await this.userService.create(createUserDto);
   }
-
+  
   @Get()
   findAll() {
     return this.userService.findAll();
@@ -26,7 +35,7 @@ export class UsersController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
-  }
+}
 
   @Delete(':id')
   remove(@Param('id') id: string) {
